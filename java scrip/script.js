f28b1b2bc64a49fbaef2bffff1a33a7d11795d29
Rithanya <rithanyaloganathan07@gmail.com>
2025-06-06 16:44:02 +0530

    hello

---
+++ java scrip/script.js
@@ -0,0 +1,20 @@
+function sum(){
+    console.log(a+b)
+
+}
+var sum=(a,b)=>{
+    return a+b;
+
+} 
+console.log(sum(10,20))
+var arr =[10,20,30,40]
+var arr2 =[...arr,50,60,70]
+console.log(arr)
+console.log(arr2) 
+
+var a=10
+var b=20
+var sum=a+b
+console .log(a,"+",b,"=",sum)
+console.log(`${a}+${b}=${sum}`)
+
