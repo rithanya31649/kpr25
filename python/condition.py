d5ced3e26280a4ce299c303a2fcc4e1a0508a16f
Rithanya <rithanyaloganathan07@gmail.com>
2025-06-09 12:56:03 +0530

    hello

---
+++ python/condition.py
@@ -0,0 +1,28 @@
+a = 10
+b = 20
+c=30
+if a > b and a >c:
+    print("a is greater")
+elif b>a and b>c:
+    print("b is greater")
+
+else:
+    print("c is greater")    
+for i in range(1,6,2):
+      print(i)
+def add():
+    print(30+50)
+add()
+list={1,2,3,4,5}
+for i in list:
+    print(i)
+
+    tuple = (5,8,4,9);
+    for i in tuple:
+        print(i)    
+
+dictionary = {"name":"rithanya","password":"123456"}
+print(dictionary["name"])
+print(type(dictionary))
+for key in dictionary:
+    print(dictionary[key])
